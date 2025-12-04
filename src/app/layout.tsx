c85c69289c1abad60a507c9bfd9478407ebad603
Yead191 <yead191@gmail.com>
2025-12-04 18:14:00 +0600

    updated google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,33 @@ export default function RootLayout({
   return (
     <AntProvider>
       <html lang="en">
+        <head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function (w, d, s, i, l) {
+          w[l] = w[l] || [];
+          w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+          var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = 'dataLayer' != 'dataLayer' ? '&l=' + 1 : '';
+          j.async = true;
+          j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'GTM-KDW94K5B', 'datalayer');`,
+            }}
+          />
+        </head>
         <body className={`${poppins.variable}  antialiased`}>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KDW94K5B"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Toaster position="top-center" duration={2000} />
           {children}
         </body>
