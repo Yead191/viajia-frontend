962d8cd0f6e5a6638e571a89e28d59b77993cd64
Yead191 <yead191@gmail.com>
2025-12-05 21:13:00 +0600

    bug fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import AntProvider from "@/lib/provider/AntProvider";
-import Script from "next/script";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -29,19 +28,21 @@ export default function RootLayout({
     <AntProvider>
       <html lang="en">
         <head>
-          <Script id="gtm-base" strategy="beforeInteractive">
-            {`
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l !== 'dataLayer' ? '&l='+l : '';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KDW94K5B');
-            `}
-          </Script>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function (w, d, s, i, l) {
+          w[l] = w[l] || [];
+          w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+          var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = 'dataLayer' != 'dataLayer' ? '&l=' + 1 : '';
+          j.async = true;
+          j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+          f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'GTM-KDW94K5B', 'datalayer');`,
+            }}
+          />
         </head>
         <body className={`${poppins.variable}  antialiased`}>
           {/* Google Tag Manager (noscript) */}
@@ -53,7 +54,6 @@ export default function RootLayout({
               style={{ display: "none", visibility: "hidden" }}
             ></iframe>
           </noscript>
-
           <Toaster position="top-center" duration={2000} />
           {children}
         </body>
@@ -61,5 +61,3 @@ export default function RootLayout({
     </AntProvider>
   );
 }
-
-// nothing is commit
